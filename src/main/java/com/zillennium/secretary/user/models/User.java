50e6b87f96fda68dd7e97f8a1a4bf621e1845e8d
Zillennium Developer <thornsovannarath@gmail.com>
2020-11-16 11:59:09 +0700

    Second commit

---
+++ src/main/java/com/zillennium/secretary/user/models/User.java
@@ -0,0 +1,16 @@
+package com.zillennium.secretary.user.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+}
