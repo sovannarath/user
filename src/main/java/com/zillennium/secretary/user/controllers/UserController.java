a328780a7731a2dd317c0c0d79a5b39cf49b95fa
zillennium <thornsovannarath@gmail.com>
2020-11-17 15:51:30 +0700

    create controller, model and service

--- src/main/java/com/zillennium/secretary/user/controllers/UserController.java
+++ src/main/java/com/zillennium/secretary/user/controllers/UserController.java
@@ -1,14 +1,43 @@
 package com.zillennium.secretary.user.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.zillennium.secretary.user.models.User;
+import com.zillennium.secretary.user.services.UserService;
 
 @Controller
 public class UserController {
 	
+	@Autowired
+	private UserService userService;
+	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping("/list")
+	public ResponseEntity<Object> allUsers(){
+		return new ResponseEntity<>(userService.all(), HttpStatus.OK);
+	}
+	
+	@RequestMapping("/{id}")
+	public ResponseEntity<Object> get(@PathVariable("id") long id){
+		return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/", method= RequestMethod.POST)
+	public ResponseEntity<Object> create(@RequestBody User user){
+		return new ResponseEntity<>(userService.create(user), HttpStatus.OK);
 	}
 	
 }
--- src/main/java/com/zillennium/secretary/user/models/User.java
+++ src/main/java/com/zillennium/secretary/user/models/User.java
@@ -1,9 +1,12 @@
 package com.zillennium.secretary.user.models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -11,6 +14,161 @@ import javax.persistence.Table;
 public class User {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
+	private String name;
+	private String email;
+	private String email_verified_at;
+	private String password;
+	private String remember_token;
+	private String created_at;
+	private String updated_at;
+	private long role;
+	private String position;
+	private String phone_number;
+	private String date_of_birth;
+	private enum gender{MALE, FEMALE};
+	private byte status;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "reference", referencedColumnName = "id")
+	private User reference;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(long id, String name, String email, String email_verified_at, String password, String remember_token,
+			String created_at, String updated_at, long role, String position, String phone_number, String date_of_birth,
+			byte status, User reference) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.email_verified_at = email_verified_at;
+		this.password = password;
+		this.remember_token = remember_token;
+		this.created_at = created_at;
+		this.updated_at = updated_at;
+		this.role = role;
+		this.position = position;
+		this.phone_number = phone_number;
+		this.date_of_birth = date_of_birth;
+		this.status = status;
+		this.reference = reference;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail_verified_at() {
+		return email_verified_at;
+	}
+
+	public void setEmail_verified_at(String email_verified_at) {
+		this.email_verified_at = email_verified_at;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRemember_token() {
+		return remember_token;
+	}
+
+	public void setRemember_token(String remember_token) {
+		this.remember_token = remember_token;
+	}
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+
+	public String getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(String updated_at) {
+		this.updated_at = updated_at;
+	}
+
+	public long getRole() {
+		return role;
+	}
+
+	public void setRole(long role) {
+		this.role = role;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getPhone_number() {
+		return phone_number;
+	}
+
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+
+	public String getDate_of_birth() {
+		return date_of_birth;
+	}
+
+	public void setDate_of_birth(String date_of_birth) {
+		this.date_of_birth = date_of_birth;
+	}
+
+	public byte getStatus() {
+		return status;
+	}
+
+	public void setStatus(byte status) {
+		this.status = status;
+	}
+
+	public User getReference() {
+		return reference;
+	}
+
+	public void setReference(User reference) {
+		this.reference = reference;
+	}
+	
 }
