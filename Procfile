b77b5b5d7d782b0c2a7301725f8e2ab8711a7630
sovannarath <thornsovannarath@gmail.com>
2021-05-13 15:30:54 +0700

    update code

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: ./mvnw flyway:migrate
-release: java -cp target/user-1.0.0.jar:target/dependency/* con.zillennium.secretary.user.Migrations
+release: java -cp target/user-1.0.0.jar:target/dependency/* com.zillennium.secretary.user.Migrations
--- src/main/java/com/zillennium/secretary/user/Migrations.java
+++ src/main/java/com/zillennium/secretary/user/Migrations.java
@@ -5,9 +5,6 @@ import org.flywaydb.core.Flyway;
 public class Migrations {
     public static void main(String[] args) throws Exception {
         Flyway flyway = Flyway.configure().dataSource("jdbc:postgres://bektsdkfviscye:8ecbe672b117dce4a29a99ead950c39fa60b1fffacd733973cbefc9e0e6fc754@ec2-52-21-153-207.compute-1.amazonaws.com:5432/d8jhoof9ujfbef", "bektsdkfviscye", "8ecbe672b117dce4a29a99ead950c39fa60b1fffacd733973cbefc9e0e6fc754").load();
-        /*flyway.setDataSource(System.getenv(),
-                             System.getenv(),
-                             System.getenv());*/
         flyway.migrate();
     }
 }
