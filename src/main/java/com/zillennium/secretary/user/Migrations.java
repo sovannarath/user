4ba460519595b7864edf0fff5cda23eb970339cc
sovannarath <thornsovannarath@gmail.com>
2021-05-13 16:32:08 +0700

    update code

--- src/main/java/com/zillennium/secretary/user/Migrations.java
+++ src/main/java/com/zillennium/secretary/user/Migrations.java
@@ -5,9 +5,9 @@ import org.flywaydb.core.Flyway;
 public class Migrations {
     public static void main(String[] args) throws Exception {
     	Flyway flyway = new Flyway();
-        flyway.setDataSource(System.getenv("jdbc:postgres://bektsdkfviscye:8ecbe672b117dce4a29a99ead950c39fa60b1fffacd733973cbefc9e0e6fc754@ec2-52-21-153-207.compute-1.amazonaws.com:5432/d8jhoof9ujfbef"),
-                             System.getenv("bektsdkfviscye"),
-                             System.getenv("8ecbe672b117dce4a29a99ead950c39fa60b1fffacd733973cbefc9e0e6fc754"));
+        flyway.setDataSource("jdbc:postgres://bektsdkfviscye:8ecbe672b117dce4a29a99ead950c39fa60b1fffacd733973cbefc9e0e6fc754@ec2-52-21-153-207.compute-1.amazonaws.com:5432/d8jhoof9ujfbef",
+                             "bektsdkfviscye",
+                             "8ecbe672b117dce4a29a99ead950c39fa60b1fffacd733973cbefc9e0e6fc754");
         flyway.migrate();
     	/*Flyway flyway = Flyway.configure().dataSource("jdbc:postgres://bektsdkfviscye:8ecbe672b117dce4a29a99ead950c39fa60b1fffacd733973cbefc9e0e6fc754@ec2-52-21-153-207.compute-1.amazonaws.com:5432/d8jhoof9ujfbef", "bektsdkfviscye", "8ecbe672b117dce4a29a99ead950c39fa60b1fffacd733973cbefc9e0e6fc754").load();
         flyway.migrate();*/
