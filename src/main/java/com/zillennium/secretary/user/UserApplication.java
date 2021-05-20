92f9c412db9f7c1c7ef264fca3450aa1b751e560
sovannarath <thornsovannarath@gmail.com>
2021-05-20 16:43:45 +0700

    Update Code

--- src/main/java/com/zillennium/secretary/user/UserApplication.java
+++ src/main/java/com/zillennium/secretary/user/UserApplication.java
@@ -3,11 +3,26 @@ package com.zillennium.secretary.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.zillennium.secretary.user.services.UploadService.FilesStorageService;
+
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+
 @SpringBootApplication
-public class UserApplication {
+public class UserApplication implements CommandLineRunner{
+	
+	@Resource
+	FilesStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... arg) throws Exception {
+	   storageService.deleteAll();
+	   storageService.init();
 	}
 
 }
---
+++ src/main/java/com/zillennium/secretary/user/services/UploadService/FilesStorageService.java
@@ -0,0 +1,14 @@
+package com.zillennium.secretary.user.services.UploadService;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FilesStorageService {
+	  public void init();
+	  public void save(MultipartFile file);
+	  public Resource load(String filename);
+	  public void deleteAll();
+	  public Stream<Path> loadAll();
+}
